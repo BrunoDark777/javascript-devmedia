ff83852e032e60dae7248180f006c439a876f4d8
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2024-11-27 03:18:16 -0300

    Manipulando String
    Eliminando o espaço entre a String usando o método nativo trim()

---
+++ exemplos/trim2.js
@@ -0,0 +1,8 @@
+let cep_acesso = "  29880-500   ";
+let cep_cadastro = "29880-500";
+
+if(cep_acesso.trim() == cep_cadastro.trim()){
+    console.log("Os ceps são iguais");
+} else{
+    console.log("Os ceps são diferentes");
+}
