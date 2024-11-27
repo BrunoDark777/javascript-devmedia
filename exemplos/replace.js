c4e8d682a02a0cfae7306b39345e5bceb9e349c3
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2024-11-27 04:09:08 -0300

    Substituindo strings com replace()
    O método replace() é a melhor forma de substituir um termo dentro de um texto na linguagem JavaScript.

---
+++ exemplos/replace.js
@@ -0,0 +1,7 @@
+let texto = "O Rio de Janeiro é uma das cidades mais bonitas do mundo! Essa cidade possui muitos pontos turísticos e inúmeras opções de lazer. Quem vive nessa cidade é muito privilegiado!";
+
+let resultado = texto.replace("Rio de Janeiro", "RJ");
+
+console.log(resultado);
+
+//O método replace() é a melhor forma de substituir um termo dentro de um texto na linguagem JavaScript.
