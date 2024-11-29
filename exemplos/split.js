80999c6ecb217b7c2a48135c09451af2b9593bb4
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2024-11-28 21:46:21 -0300

    Método split
    O método split() é muito útil para extrair dados de strings maiores. Assim é importante saber como é a composição da string para identificar o caractere que será usado como separador

---
+++ exemplos/split.js
@@ -0,0 +1,8 @@
+let nome_completo = "Ana Mendes da Silva";
+let array = nome_completo.split(" ");
+
+let nome = array[0];
+
+console.log("Ola " +nome+ ", Seja Bem vindo(a)");
+
+// O método split() é muito útil para extrair dados de strings maiores. Assim é importante saber como é a composição da string para identificar o caractere que será usado como separador
