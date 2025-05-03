fa1cee1a02415d816fe7843849cde124e805d80d
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2025-05-03 11:43:36 -0300

    Funções sem parâmetro
    Funções sem parâmetro não vão receber valores para executar em seus blocos de código.

---
+++ exemplos-2/funcoes.js
@@ -0,0 +1,10 @@
+//Funções sem parâmetro
+
+function retorna_versao() // Nenhum parâmetro foi passado para a função
+{
+    return 1.1; // Retorno da versão
+}
+
+let versao = retorna_versao(); // Atribuímos na variável retorna_versao a função que nos retorna a versão de uma aplicação
+
+console.log(versao); // E por fim, imprimimos a variável
