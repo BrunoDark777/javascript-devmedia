e9d69ebe6ac7bc9a0672775008ec2364e9c98b66
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2025-05-16 00:56:49 -0300

    correção de bugs

--- exemplos-2/signodehoje.js
+++ exemplos-2/signodehoje.js
@@ -13,20 +13,35 @@ let colecao_signos = [
     {"Nome": "Capricórnio", "DataInicio": "12-22", "dataFim": "01-19"}
 ];
 
-const retorna_signo = (signos) => {
-
-    let data = new Date("2020-02-07 00:00:00");
+const retorna_signo = (signos, data) => {
 
     let ano = data.getFullYear();
 
-    let data_inicio_signo = new Date(ano + "-" + signos[0]["DataInicio"] + "00:00:00");
-    let data_fim_signo = new Date(ano + "-" + signos[0]["dataFim"] + "23:59:59");
+    for(const signo of signos){
 
-    if(data >= data_inicio_signo && data <= data_fim_signo){
-        return signos[0]["Nome"];
+        let data_inicio_signo = new Date(ano + "-" + signo["DataInicio"] + " 00:00:00");
+    let data_fim_signo = new Date(ano + "-" + signo["dataFim"] + " 23:59:59");
+    
+    let tipo_comparacao = signo["DataInicio"] == "12-22" ? "or" : "and";
+
+    if(tipo_comparacao == "and"){
+
+        if(data >= data_inicio_signo && data <= data_fim_signo){
+            return signo["Nome"];
+        }
+    } else if(tipo_comparacao == "or"){
+
+        if(data >= data_inicio_signo || data <= data_fim_signo){
+            return signo["Nome"];
+        }
     }
+
+    }
+    
 }
 
-const nome_signo = retorna_signo(signos);
+let data_app = new Date("2020-05-08 00:00:00");
+
+const nome_signo = retorna_signo(colecao_signos, data_app);
 
 console.log("O signo do dia é: " + nome_signo);
