89cf2477195f7bfd8780abffed161103c787a773
BrunoDark777 <106707123+BrunoDark777@users.noreply.github.com>
2025-07-29 00:44:15 -0300

    Separando contextos em JavaScript

---
+++ exemplos-2/contextos-apli.js
@@ -0,0 +1,72 @@
+//Contextos da Aplicação:
+
+//Contexto de validação de peso e altura.
+
+
+function validaPeso(peso){
+    let pesoValido = false;
+
+    if(peso > 0){
+        pesoValido = true;
+    }
+
+    return pesoValido;
+}
+
+function validaAltura(altura){
+    let alturaValida = false;
+
+    if(altura > 0){
+        alturaValida = true;
+    }
+
+    return alturaValida;
+}
+
+//Contextto do cálculo de IMC.
+
+// O contexto do cálculo de IMC é representado pela funçao calculaIMC, pois o único objetivo dela é esse.
+function calculaIMC(peso, altura){
+    let imc = peso / (altura * altura);
+    return imc;
+}
+
+// A função retornaStatusIMC é utilizada para retornar o status do usúario com base no cálculo do IMC e por esse motivo ela está no contexto de cálculo IMC.
+function retornaStatusIMC(imc){
+    let status;
+
+    if(imc < 18.5){
+        status = 'Abaixo do peso';
+    } else if(imc > 18.5 && imc < 24.9){
+        status = 'Peso normal';
+    } else if(imc > 24.9 && imc < 30){
+        status = 'Acima do peso';
+    } else {
+        status = 'Thais Carla, é você???'
+    }
+
+    return status;
+}
+
+//Contexto de exibição.
+
+// O restante do código assume o contexto de exibição, pois seu objetivo em conjunto é retornar o resultado do cálculo de IMC para o usuário.
+let peso = 78;
+let altura = 1.72;
+
+let verificaPesoValido = pesoValido(peso);
+let verificaAlturaValida = alturaValida(altura);
+
+if(verificaPesoValido && verificaAlturaValida){
+    
+let resultado = calculaIMC(peso, altura);
+let statusIMC = retornaStatusIMC(resultado)
+
+console.log("Seu IMC é "+resultado+" e você está "+statusIMC);
+} else {
+    console.log("Peso e altura devem ser maiores que zero");
+}
+
+// Veja que mesmo em códigos mais complexos é possível identificar contextos bem definidos na sua estrutura.
+
+// Em resumo, para dividir uma aplicação em camadas, basta você separar os blocos de código de acordo com seus contextos.
---
+++ exemplos-2/sepa-contextos.js
@@ -0,0 +1,14 @@
+// Separando contextos:
+
+// Contexto do cálculo da área.
+
+//Essa três linhas representam o contexto do cálculo da área pois todas elas são utilizadas no cálculo da área do triângulo.
+let base = 16;
+let altura = 8;
+
+let area = (base * altura) / 2;
+
+// contexto de exibição.
+
+//O único objetivo dessa linha de código é exibir o resultado final, por isso ela fica no contexto de exibição.
+console.log("A área do triangulo é "+area);
